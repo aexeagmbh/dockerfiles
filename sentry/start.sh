61cae11d195bc2833eb2265283221a91dd59a457
Arne Schauf <git@as-webservices.de>
2015-01-28 12:04:33 +0100

    startscript fix

--- sentry/start.sh
+++ sentry/start.sh
@@ -3,6 +3,6 @@
 sentry --config=/etc/sentry.conf.py upgrade
 
 if [ "$1" -eq 'worker' ]; then
-    sentry --config=/etc/sentry.conf.py start
-else
     sentry --config=/etc/sentry.conf.py celery worker -B
+else
+    sentry --config=/etc/sentry.conf.py start
