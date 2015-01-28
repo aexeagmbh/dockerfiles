9fdcac9097d622ff667216d637884f9c74ef2b52
Arne Schauf <git@as-webservices.de>
2015-01-28 11:58:17 +0100

    some fixes to the sentry stuff

--- sentry/start.sh
+++ sentry/start.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
 sentry --config=/etc/sentry.conf.py upgrade
-sentry --config=/etc/sentry.conf.py start
+
+if [ "$1" -eq 'worker' ]; then
+    sentry --config=/etc/sentry.conf.py start
+else
+    sentry --config=/etc/sentry.conf.py celery worker -B
