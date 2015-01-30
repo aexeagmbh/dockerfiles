20a969de8f08d4f0c4e6d792d97106bc3f3f4fb4
Arne Schauf <git@as-webservices.de>
2015-01-30 15:31:04 +0100

    add worker option

--- wagtail/project/start.sh
+++ wagtail/project/start.sh
@@ -5,4 +5,8 @@ sleep 5
 python3 manage.py migrate --noinput
 python3 manage.py runscript create_admin
 # python3 manage.py runserver 0.0.0.0:8000
-gunicorn project.wsgi:application --log-level=debug --log-file=/log/gunicorn.log -b 0.0.0.0:8000
+if [ "$1" -eq 'worker' ]; then
+    celery -n wagtail.%h -A project worker --loglevel=info
+else
+    gunicorn project.wsgi:application --log-level=debug --log-file=/log/gunicorn.log -b 0.0.0.0:8000
+fi
