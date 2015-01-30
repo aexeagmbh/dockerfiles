584ce711a74c800415ddfcabd908494ba4473e7a
Arne Schauf <git@as-webservices.de>
2015-01-30 17:10:14 +0100

    fix string comparison in sentry

--- sentry/start.sh
+++ sentry/start.sh
@@ -3,7 +3,7 @@
 echo "Starting sentry $1"
 sentry --config=/etc/sentry.conf.py upgrade
 
-if [ "$1" -eq 'worker' ]; then
+if [ "$1" = 'worker' ]; then
     sentry --config=/etc/sentry.conf.py celery worker -B
 else
     sentry --config=/etc/sentry.conf.py start
