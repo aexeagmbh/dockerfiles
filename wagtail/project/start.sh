d3b34e9882283806b439934e4791c659dc80854d
Arne Schauf <git@as-webservices.de>
2015-02-02 17:30:39 +0100

    compress on startup

--- wagtail/project/start.sh
+++ wagtail/project/start.sh
@@ -2,6 +2,7 @@
 
 python3 manage.py migrate --noinput
 python3 manage.py runscript create_admin
+python3 manage.py compress
 python3 manage.py collectstatic --noinput
 if [ "$1" = 'worker' ]; then
     celery -n wagtail.%h -A project worker --loglevel=info
