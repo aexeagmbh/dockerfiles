8834d712e80e9901e6dbe9341b0d75c7d86c232e
Arne Schauf <git@as-webservices.de>
2015-02-03 15:03:55 +0100

    wagtail templates: make header intro configurable

--- wagtail/project/core/models.py
+++ wagtail/project/core/models.py
@@ -8,7 +8,24 @@ from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 
-class HomePage(Page):
+class AxPage(Page):
+    header_title = models.CharField(max_length=512, blank=True)
+    header_slogan = models.CharField(max_length=512, blank=True)
+
+    class Meta:
+        abstract = True
+
+AX_BASE_FIELDS = [
+    MultiFieldPanel(
+        [
+            FieldPanel('header_title', classname="full"),
+            FieldPanel('header_slogan', classname="full"),
+        ],
+        heading='Header elements', classname="collapsible collapsed"),
+]
+
+
+class HomePage(AxPage):
     block1_title = models.CharField(max_length=512, blank=True)
     block1_subtitle = RichTextField(blank=True)
     block1_content1 = RichTextField(blank=True)
@@ -68,45 +85,45 @@ class HomePage(Page):
 
 
 HOME_BLOCK1_FIELDS = [
-    FieldPanel('block1_title'),
-    FieldPanel('block1_subtitle'),
-    FieldPanel('block1_content1'),
-    FieldPanel('block1_content2'),
-    FieldPanel('block1_content3'),
-    FieldPanel('block1_content4'),
-    FieldPanel('block1_footer'),
+    FieldPanel('block1_title', classname="full"),
+    FieldPanel('block1_subtitle', classname="full"),
+    FieldPanel('block1_content1', classname="full"),
+    FieldPanel('block1_content2', classname="full"),
+    FieldPanel('block1_content3', classname="full"),
+    FieldPanel('block1_content4', classname="full"),
+    FieldPanel('block1_footer', classname="full"),
 ]
 
 HOME_BLOCK2_FIELDS = [
-    FieldPanel('block2_title'),
-    FieldPanel('block2_content1'),
-    FieldPanel('block2_content2'),
-    FieldPanel('block2_content3'),
-    FieldPanel('block2_content4'),
+    FieldPanel('block2_title', classname="full"),
+    FieldPanel('block2_content1', classname="full"),
+    FieldPanel('block2_content2', classname="full"),
+    FieldPanel('block2_content3', classname="full"),
+    FieldPanel('block2_content4', classname="full"),
 ]
 
 HOME_BLOCK3_FIELDS = [
-    FieldPanel('block3_content1'),
-    FieldPanel('block3_thumbnail1'),
-    FieldPanel('block3_content2'),
-    FieldPanel('block3_thumbnail2'),
-    FieldPanel('block3_content3'),
-    FieldPanel('block3_thumbnail3'),
-    FieldPanel('block3_content4'),
-    FieldPanel('block3_thumbnail4'),
+    FieldPanel('block3_content1', classname="full"),
+    FieldPanel('block3_thumbnail1', classname="full"),
+    FieldPanel('block3_content2', classname="full"),
+    FieldPanel('block3_thumbnail2', classname="full"),
+    FieldPanel('block3_content3', classname="full"),
+    FieldPanel('block3_thumbnail3', classname="full"),
+    FieldPanel('block3_content4', classname="full"),
+    FieldPanel('block3_thumbnail4', classname="full"),
 ]
 
 HOME_BLOCK_SIGNUP_FIELDS = [
-    FieldPanel('block_signup_title'),
-    FieldPanel('block_signup_button_label'),
+    FieldPanel('block_signup_title', classname="full"),
+    FieldPanel('block_signup_button_label', classname="full"),
 ]
 
 HOME_BLOCK4_FIELDS = [
-    FieldPanel('block4_content1'),
-    FieldPanel('block4_content2'),
+    FieldPanel('block4_content1', classname="full"),
+    FieldPanel('block4_content2', classname="full"),
 ]
 
-HomePage.content_panels = [
+HomePage.content_panels = AX_BASE_FIELDS + [
     FieldPanel('title', classname="full title"),
     MultiFieldPanel(HOME_BLOCK1_FIELDS, heading='Block 1 elements', classname="collapsible collapsed"),
     MultiFieldPanel(HOME_BLOCK2_FIELDS, heading='Block 2 elements', classname="collapsible collapsed"),
@@ -116,7 +133,7 @@ HomePage.content_panels = [
 ]
 
 
-class OneColumnMainPage(Page):
+class OneColumnMainPage(AxPage):
     subtitle = RichTextField()
     row1_teaser1 = RichTextField(blank=True)
     row1_teaser2 = RichTextField(blank=True)
@@ -128,13 +145,37 @@ class OneColumnMainPage(Page):
     foot_row = RichTextField(blank=True)
 
 
-OneColumnMainPage.content_panels = [
+OneColumnMainPage.content_panels = AX_BASE_FIELDS + [
     FieldPanel('title', classname="full title"),
     FieldPanel('subtitle', classname="full"),
     FieldPanel('row1_teaser1', classname="full"),
     FieldPanel('row1_teaser2', classname="full"),
     FieldPanel('row1_teaser3', classname="full"),
     FieldPanel('main_content', classname="full"),
+    FieldPanel('conversion_title', classname="full"),
+    FieldPanel('conversion_url', classname="full"),
+    FieldPanel('conversion_button_label', classname="full"),
+    FieldPanel('foot_row', classname="full"),
+]
+
+
+class TwoColumnMainPage(AxPage):
+    subtitle = RichTextField()
+    teaser = RichTextField(blank=True)
+    main_content_left = RichTextField()
+    main_content_right = RichTextField()
+    conversion_title = models.CharField(max_length=256, blank=True)
+    conversion_url = models.URLField(blank=True)
+    conversion_button_label = models.CharField(max_length=256, blank=True)
+    foot_row = RichTextField(blank=True)
+
+
+TwoColumnMainPage.content_panels = AX_BASE_FIELDS + [
+    FieldPanel('title', classname="full title"),
+    FieldPanel('subtitle', classname="full"),
+    FieldPanel('teaser', classname="full"),
+    FieldPanel('main_content_left', classname="full"),
+    FieldPanel('main_content_right', classname="full"),
     FieldPanel('conversion_title', classname="full"),
     FieldPanel('conversion_url', classname="full"),
     FieldPanel('conversion_button_label', classname="full"),
