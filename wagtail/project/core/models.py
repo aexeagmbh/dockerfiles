95301ff8ff9326cc2002ea6e1021fd23e88754c6
Arne Schauf <git@as-webservices.de>
2015-02-03 11:53:21 +0100

    add one column main page template

--- wagtail/project/core/models.py
+++ wagtail/project/core/models.py
@@ -67,7 +67,7 @@ class HomePage(Page):
         verbose_name = "Homepage"
 
 
-BLOCK1_FIELDS = [
+HOME_BLOCK1_FIELDS = [
     FieldPanel('block1_title'),
     FieldPanel('block1_subtitle'),
     FieldPanel('block1_content1'),
@@ -77,7 +77,7 @@ BLOCK1_FIELDS = [
     FieldPanel('block1_footer'),
 ]
 
-BLOCK2_FIELDS = [
+HOME_BLOCK2_FIELDS = [
     FieldPanel('block2_title'),
     FieldPanel('block2_content1'),
     FieldPanel('block2_content2'),
@@ -85,7 +85,7 @@ BLOCK2_FIELDS = [
     FieldPanel('block2_content4'),
 ]
 
-BLOCK3_FIELDS = [
+HOME_BLOCK3_FIELDS = [
     FieldPanel('block3_content1'),
     FieldPanel('block3_thumbnail1'),
     FieldPanel('block3_content2'),
@@ -96,21 +96,47 @@ BLOCK3_FIELDS = [
     FieldPanel('block3_thumbnail4'),
 ]
 
-BLOCK_SIGNUP_FIELDS = [
+HOME_BLOCK_SIGNUP_FIELDS = [
     FieldPanel('block_signup_title'),
     FieldPanel('block_signup_button_label'),
 ]
 
-BLOCK4_FIELDS = [
+HOME_BLOCK4_FIELDS = [
     FieldPanel('block4_content1'),
     FieldPanel('block4_content2'),
 ]
 
 HomePage.content_panels = [
     FieldPanel('title', classname="full title"),
-    MultiFieldPanel(BLOCK1_FIELDS, heading='Block 1 elements', classname="collapsible collapsed"),
-    MultiFieldPanel(BLOCK2_FIELDS, heading='Block 2 elements', classname="collapsible collapsed"),
-    MultiFieldPanel(BLOCK3_FIELDS, heading='Block 3 elements', classname="collapsible collapsed"),
-    MultiFieldPanel(BLOCK_SIGNUP_FIELDS, heading='Block Sign Up elements', classname="collapsible collapsed"),
-    MultiFieldPanel(BLOCK4_FIELDS, heading='Block 4 elements', classname="collapsible collapsed"),
+    MultiFieldPanel(HOME_BLOCK1_FIELDS, heading='Block 1 elements', classname="collapsible collapsed"),
+    MultiFieldPanel(HOME_BLOCK2_FIELDS, heading='Block 2 elements', classname="collapsible collapsed"),
+    MultiFieldPanel(HOME_BLOCK3_FIELDS, heading='Block 3 elements', classname="collapsible collapsed"),
+    MultiFieldPanel(HOME_BLOCK_SIGNUP_FIELDS, heading='Block Sign Up elements', classname="collapsible collapsed"),
+    MultiFieldPanel(HOME_BLOCK4_FIELDS, heading='Block 4 elements', classname="collapsible collapsed"),
+]
+
+
+class OneColumnMainPage(Page):
+    subtitle = RichTextField()
+    row1_teaser1 = RichTextField(blank=True)
+    row1_teaser2 = RichTextField(blank=True)
+    row1_teaser3 = RichTextField(blank=True)
+    main_content = RichTextField()
+    conversion_title = models.CharField(max_length=256, blank=True)
+    conversion_url = models.URLField(blank=True)
+    conversion_button_label = models.CharField(max_length=256, blank=True)
+    foot_row = RichTextField(blank=True)
+
+
+OneColumnMainPage.content_panels = [
+    FieldPanel('title', classname="full title"),
+    FieldPanel('subtitle', classname="full"),
+    FieldPanel('row1_teaser1', classname="full"),
+    FieldPanel('row1_teaser2', classname="full"),
+    FieldPanel('row1_teaser3', classname="full"),
+    FieldPanel('main_content', classname="full"),
+    FieldPanel('conversion_title', classname="full"),
+    FieldPanel('conversion_url', classname="full"),
+    FieldPanel('conversion_button_label', classname="full"),
+    FieldPanel('foot_row', classname="full"),
 ]
