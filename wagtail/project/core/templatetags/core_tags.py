060cb6027767e508a46e4df6ea4d5f7973af87dd
Arne Schauf <git@as-webservices.de>
2015-02-16 18:09:31 +0100

    add submenu

--- wagtail/project/core/templatetags/core_tags.py
+++ wagtail/project/core/templatetags/core_tags.py
@@ -5,8 +5,6 @@ register = template.Library()
 
 @register.assignment_tag(takes_context=True)
 def get_site_root(context):
-    # NB this returns a core.Page, not the implementation-specific model used
-    # so object-comparison to self will return false as objects would differ
     return context['request'].site.root_page
 
 
@@ -17,21 +15,36 @@ def has_menu_children(page):
         return False
 
 
-# Retrieves the top menu items - the immediate children of the parent page
-# The has_menu_children method is necessary because the bootstrap menu requires
-# a dropdown class to be applied to a parent
 @register.inclusion_tag('core/tags/top_menu.html', takes_context=True)
 def top_menu(context, parent, calling_page=None):
-    menuitems = [parent]
-    menuitems.extend(list(parent.get_children().filter(
+    menuitems = list(parent.get_children().filter(
         live=True,
         show_in_menus=True
-    )))
+    ))
     for menuitem in menuitems:
         menuitem.show_dropdown = has_menu_children(menuitem)
+        menuitem.active = (False if calling_page is None
+                           else calling_page.url.startswith(menuitem.url))
+    menuitems = [parent] + menuitems
     return {
         'calling_page': calling_page,
         'menuitems': menuitems,
-        # required by the pageurl tag that we want to use within this template
+        'request': context['request'],
+    }
+
+
+@register.inclusion_tag('core/tags/sub_menu.html', takes_context=True)
+def sub_menu(context, parent, calling_page=None):
+    menuitems = list(parent.get_children().filter(
+        live=True,
+        show_in_menus=True
+    ))
+    for menuitem in menuitems:
+        menuitem.show_dropdown = has_menu_children(menuitem)
+        menuitem.active = (False if calling_page is None
+                           else calling_page.url.startswith(menuitem.url))
+    return {
+        'calling_page': calling_page,
+        'menuitems': menuitems,
         'request': context['request'],
     }
